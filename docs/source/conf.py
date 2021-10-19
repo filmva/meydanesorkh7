afe727e33f40f99906084568e3ce13bcc84b77c7
filmva <92706311+filmva@users.noreply.github.com>
2021-10-19 21:08:12 +0330

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,11 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'meydanesorkh 7'
+copyright = '2021, meydanesorkh7'
+author = 'meydanesorkh 7'
 
-release = '0.1'
-version = '0.1.0'
+
 
 # -- General configuration
 
